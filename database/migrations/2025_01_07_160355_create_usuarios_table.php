86b4e1393067c2766849529f6003ca98b6a9d2ac
Eva Gandia Belda <147039955+evagandiabelda@users.noreply.github.com>
2025-01-07 17:49:14 +0100

    Implementació de la migració per a "usuarios" i execució de la migració.

--- database/migrations/2025_01_07_160355_create_usuarios_table.php
+++ database/migrations/2025_01_07_160355_create_usuarios_table.php
@@ -4,24 +4,35 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
+class CreateUsuariosTable extends Migration
 {
-    /**
-     * Run the migrations.
-     */
-    public function up(): void
+    public function up()
     {
         Schema::create('usuarios', function (Blueprint $table) {
             $table->id();
-            $table->timestamps();
+            $table->string('nombre_completo');
+            $table->string('nombre_usuario')->unique();
+            $table->string('email')->unique();
+            $table->string('password');
+            $table->binary('foto')->nullable();
+            // $table->unsignedBigInteger('id_lista_spices')->nullable();
+            $table->text('descripcion_perfil')->nullable();
+            $table->json('lista_shares_publicados')->nullable();
+            $table->json('lista_shares_guardados')->nullable();
+            // $table->unsignedBigInteger('id_lista_comentarios')->nullable();
+            $table->json('lista_notificaciones')->nullable();
+            $table->boolean('perfil_privado')->default(false);
+
+            // Claves foráneas (si las tablas referenciadas ya existen)
+            // $table->foreign('id_lista_spices')->references('id')->on('usuarios_spices')->onDelete('cascade');
+            // $table->foreign('id_lista_comentarios')->references('id')->on('usuarios_comentarios')->onDelete('cascade');
+
+            $table->timestamps(); // created_at y updated_at
         });
     }
 
-    /**
-     * Reverse the migrations.
-     */
-    public function down(): void
+    public function down()
     {
         Schema::dropIfExists('usuarios');
     }
-};
+}
