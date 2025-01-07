c85ddcb34f723cec3adbecdcab1618e219571422
Eva Gandia Belda <147039955+evagandiabelda@users.noreply.github.com>
2025-01-07 18:19:27 +0100

    Implementació de la ruta "usuarios".

--- routes/api.php
+++ routes/api.php
@@ -3,9 +3,9 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+use App\Http\Controllers\Api\UsuarioController;
+
+Route::apiResource('usuarios', UsuarioController::class);
 
 /* Fallback Route (404): */
 
