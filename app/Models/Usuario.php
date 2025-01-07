718ec9fc61389bc62e9e95a9ccf6b9b4343bd8ea
Eva Gandia Belda <147039955+evagandiabelda@users.noreply.github.com>
2025-01-07 17:53:35 +0100

    Implementació del model "usuario".

--- app/Models/Usuario.php
+++ app/Models/Usuario.php
@@ -7,6 +7,37 @@ use Illuminate\Database\Eloquent\Model;
 
 class Usuario extends Model
 {
-    /** @use HasFactory<\Database\Factories\UsuarioFactory> */
     use HasFactory;
+
+    // Nombre de la tabla en MySQL:
+    protected $table = 'usuarios';
+
+    // Atributos que se pueden asignar en masa:
+    protected $fillable = [
+        'nombre_completo',
+        'nombre_usuario',
+        'email',
+        'password',
+        'foto',
+        // 'id_lista_spices',
+        'descripcion_perfil',
+        'lista_shares_publicados',
+        'lista_shares_guardados',
+        // 'id_lista_comentarios',
+        'lista_notificaciones',
+        'perfil_privado',
+    ];
+
+/* 
+    // Relación con otras tablas:
+    public function listaSpices()
+    {
+        return $this->belongsTo(UsuarioSpice::class, 'id_lista_spices');
+    }
+
+    public function listaComentarios()
+    {
+        return $this->belongsTo(UsuarioComentario::class, 'id_lista_comentarios');
+    }
+*/
 }
