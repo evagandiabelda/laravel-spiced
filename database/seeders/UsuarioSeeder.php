8781117fc59eec3a64037c02e1ed48e0cbafc450
Eva Gandia Belda <147039955+evagandiabelda@users.noreply.github.com>
2025-01-07 17:55:09 +0100

    Implementació del seeder per a "usuario".

--- database/seeders/UsuarioSeeder.php
+++ database/seeders/UsuarioSeeder.php
@@ -2,16 +2,13 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\Usuario;
 
 class UsuarioSeeder extends Seeder
 {
-    /**
-     * Run the database seeds.
-     */
-    public function run(): void
+    public function run()
     {
-        //
+        Usuario::factory()->count(10)->create();
     }
 }
